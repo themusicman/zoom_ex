6c420beb1fe46289f4b02fa76a453631ae166f96
Thomas Brewer <tom.brewer@mac.com>
2023-09-24 05:00:59 -0400

    allow for the passing of an optional session_key

--- lib/zoom/auth/token.ex
+++ lib/zoom/auth/token.ex
@@ -7,19 +7,28 @@ defmodule Zoom.Auth.Token do
         topic: topic,
         user_identity: user_identity,
         password: password,
-        role_type: role_type
+        role_type: role_type,
+        session_key: session_key
       }) do
     sdk_key = Application.get_env(:zoom_ex, :zoom_sdk_key)
 
-    case generate_and_sign(%{
-           app_key: sdk_key,
-           tpc: topic,
-           role_type: role_type,
-           # session_key: session_key
-           user_identity: user_identity,
-           version: 1,
-           pwd: password
-         }) do
+    claims = %{
+      app_key: sdk_key,
+      tpc: topic,
+      role_type: role_type,
+      user_identity: user_identity,
+      version: 1,
+      pwd: password
+    }
+
+    claims =
+      if session_key do
+        Map.put(claims, :session_key, session_key)
+      else
+        claims
+      end
+
+    case generate_and_sign(claims) do
       {:ok, token, _claims} ->
         token
 
